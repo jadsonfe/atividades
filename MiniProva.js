7e1d84b92d3fe752c199a22aeb7c7762bb9881f5
jadsonfe <126823058+jadsonfe@users.noreply.github.com>
2023-03-29 14:08:47 -0300

    Add files via upload

---
+++ MiniProva.js
@@ -0,0 +1,24 @@
+var n=1
+var m=4
+
+
+if(n>m){
+
+    while(n>m){
+        n--
+        if(n==m){
+            break
+        }
+console.log(n)
+
+        }
+}else if(m>n){
+	
+    while(m>n){
+        m--
+if(m==n){
+    break
+}
+console.log(m)
+        }
+}
